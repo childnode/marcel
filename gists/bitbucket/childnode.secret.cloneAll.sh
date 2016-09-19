ced41327ac858800d97d1b8058916b9e4cc49398
childno͡.de <c0d3@childno.de>
2016-09-19 15:37:12 +0200

    added private bitBuckets into clone script

---
+++ gists/bitbucket/childnode.secret.cloneAll.sh
@@ -0,0 +1,4 @@
+git clone git@bitbucket.org:snippets/childnode/6kAed/unpack-all-files-with-7zip-in-batch.git ~/gists/bitbucket/childnode.secret/2015-06-21_6kAed_unpack-all-files-with-7zip-in-batch
+git clone git@bitbucket.org:snippets/childnode/5yAA9/jenkins-css-logo-hack.git ~/gists/bitbucket/childnode.secret/2015-06-21_5yAA9_jenkins-css-logo-hack
+git clone git@bitbucket.org:snippets/childnode/okLyq/unityruntimemetric.git ~/gists/bitbucket/childnode.secret/2015-06-30_okLyq_UnityRuntimeMetric.cs
+git clone git@bitbucket.org:snippets/childnode/kyAAn/unity3d-editor-datetime-timespan-and.git ~/gists/bitbucket/childnode.secret/2015-06-21_kyAAn_Unity3DEditor-DateTime_TimeSpanAndProgressBars
