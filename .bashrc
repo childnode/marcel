1badd1645194ea243b84ad6633e269a8b277f597
childno͡.de <c0d3@childno.de>
2017-11-16 08:54:39 +0100

    using function with local variable scope for any .profile or .bashrc action to prevent exposed variables in user shell

--- .bashrc
+++ .bashrc
@@ -3,13 +3,18 @@
 ## non interactive shouldn't setup anything
 [[ $- != *i* ]] && return
 
-for rcTermOrOSType in '' $TERM $TERM_PROGRAM $HOSTTYPE $MACHTYPE $OSTYPE; do
-	for rcFile in ~/.bashrc.d/${rcTermOrOSType}/*; do
-		# [[ -f ${rcFile} && "$(basename ${rcFile})" == "$(basename ${rcFile} .disabled)" ]] && echo ${rcFile} && time source ${rcFile}
-		[[ -f ${rcFile} && "$(basename ${rcFile})" == "$(basename ${rcFile} .disabled)" ]] && source ${rcFile}
-	done
-done
-unset rcTermOrOSType
-unset rcFile
+loadBashRcd () {
+    local rcTermOrOSType
+    local rcFile
+    ## export DEBUG=true
+    for rcTermOrOSType in '' $TERM $TERM_PROGRAM $HOSTTYPE $MACHTYPE $OSTYPE; do
+        for rcFile in ~/.bashrc.d/${rcTermOrOSType}/*; do
+            # [[ -f ${rcFile} && "$(basename ${rcFile})" == "$(basename ${rcFile} .disabled)" ]] && echo ${rcFile} && time source ${rcFile}
+            [[ -f ${rcFile} && "$(basename ${rcFile})" == "$(basename ${rcFile} .disabled)" ]] && source ${rcFile}
+        done
+    done
+}
+
+loadBashRcd
 
 [[ -f ~/.bash_aliases ]] && source ~/.bash_aliases
--- .bashrc.d/helper/sourceCompletions.sh
+++ .bashrc.d/helper/sourceCompletions.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
-name=$1
-path=$2
-bg=
+sourceCompletions() {
+    local name=$1
+    local path=$2
+    local bg=
+    local profileStart
+    local cmdRetCode
+    local profileEnd
 
-if [ -z "$path" ]; then
-    path=$1
-fi
+    if [ -z "$path" ]; then
+        path=$1
+    fi
 
-if [ -n "$3" ]; then
-    bg="~loading bg~"
-fi
+    if [ -n "$3" ]; then
+        bg="~loading bg~"
+    fi
 
-## double wrap to hide "job finished" information in bash
-## TODO: disabled BG load because script changes and functions are not exported from subshell to the parent
-##       and therefor not working
-#(
-#    (
-        profileStart=$(date +%s)
-        if [ ! -f $path ]; then
-            cmdRetCode=128
-        else 
-            source $path
-            cmdRetCode=$?
-        fi
-        profileEnd=$(bc <<< "$(date +%s)-$profileStart")
-        [ -n "$DEBUG" -o "$cmdRetCode" -ne 0 -o -z "$bg" ] && echo -en $GREEN"\n+++ "$name" "$NO_COLOR;
-        if [ "$cmdRetCode" -eq 0 ]; then
-            [ -n "$DEBUG" -o -z "$bg" ] && echo -e $GREEN"loaded in"$NO_COLOR" (${profileEnd}s)"
-        else
-            echo -e $RED"failed to load"$NO_COLOR" (${profileEnd}s)"
-        fi
-        [ -n "$bg" ] && echo -en "$PS0 "
-#    ) &
-#);
+    ## double wrap to hide "job finished" information in bash
+    ## TODO: disabled BG load because script changes and functions are not exported from subshell to the parent
+    ##       and therefor not working
+    #(
+    #    (
+            profileStart=$(date +%s)
+            if [ ! -f $path ]; then
+                cmdRetCode=128
+            else
+                source $path
+                cmdRetCode=$?
+            fi
+            profileEnd=$(bc <<< "$(date +%s)-$profileStart")
+            [ -n "$DEBUG" ] && [ "$cmdRetCode" -ne 0 -o -z "$bg" ] && echo -en $GREEN"\n+++ "$name" "$NO_COLOR;
+            if [ "$cmdRetCode" -eq 0 ]; then
+                [ -n "$DEBUG" -a -z "$bg" ] && echo -e $GREEN"loaded in"$NO_COLOR" (${profileEnd}s)"
+            else
+                echo -e $RED"failed to load ${name}"$NO_COLOR" (${profileEnd}s)"
+            fi
+            [ -n "$DEBUG" -a -n "$bg" ] && echo -en "$PS0 "
+    #    ) &
+    #);
+}
+sourceCompletions $*
