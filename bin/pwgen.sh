d938459790b4e0f78ed985b47c4ff1937a33125d
childno͡.de <c0d3@childno.de>
2019-04-08 07:18:44 +0200

    added helper script password generator - pwgen.sh

--- bin/pwgen.sh
+++ bin/pwgen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for ((n=0;n<10;n++)); do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m -| sed -ne 2p | cut -c-8; done
+for ((n=0;n<${2:-1};n++)); do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m -| sed -ne 2p | cut -c-${1:-8}; done
